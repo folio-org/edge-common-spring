2815114a323fdaf5214b9f1b90ab9ad3c84aacfa
nirmalr-shah <nirmal_shah@epam.com>
2023-09-07 10:34:45 +0530

    EDGCMNSPR-29 : removed unnecessary file

--- src/main/java/org/folio/edgecommonspring/constant/Constants.java
+++
@@ -1,5 +0,0 @@
-package org.folio.edgecommonspring.constant;
-
-public class Constants {
-  public static final String CANNOT_RETRIEVE_OKAPI_TOKEN_FOR_TENANT = "Cannot retrieve okapi token for tenant: ";
-}
